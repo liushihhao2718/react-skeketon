e2a74367d360146af5f5a0d0202e6b735f413dd0
劉士豪 <st930112@gmail.com>
2016-07-05 16:30:58 +0800

    react first

---
+++ src/components/List.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredient = [
+  {"id":1, "text":"ham"},
+  {"id":2, "text":"cheese"},
+  {"id":3,
+    "text":"cheese2",
+    "hahaha":123
+  }
+];
+
+var List = React.createClass({
+  render: function () {
+    var listItems = ingredient.map(function(item) {
+      return <ListItem key={item.id} ingredient={item.text} />;
+    });
+    
+    return (<ul>{listItems}</ul>);
+  }
+});
+module.exports = List;
